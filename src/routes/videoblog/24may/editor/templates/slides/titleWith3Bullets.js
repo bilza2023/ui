18d9be8a50fd71b31450db4ef021ee31802204ed
bilza2023 <bilza2023@gmail.com>
2025-05-27 10:23:49 +0500

    ok slide-1

--- src/routes/videoblog/24may/editor/templates/slides/titleWith3Bullets.js
+++ src/routes/videoblog/24may/editor/templates/slides/titleWith3Bullets.js
@@ -3,23 +3,27 @@ import { TemplateToolkit as T } from "../toolkit/Toolkit";
 export default function titleWith3Bullets(theme, data) {
   const items = [];
 
-  // Heading
+  // Optional spacing config
+  const lineGap = data.lineGap ?? 100;
+  const gapBelowHeading = data.gapBelowHeading ?? 130;
+
+  // Title block
   const titleData = T.applyPreset(T.stylePresets.text.heading, {
     text: data.title,
-    y: 60,
-    x: 100 // Target final position
+    x: 100,
+    y: 60
   });
   const title = T.ItemBuilders.text(theme, titleData);
-  T.addAnimation(title, "text", "flyInRight", 1); // Animate in from right
-  title.animations[0].props.to = title.x; // Set final x as animation target
+  T.addAnimation(title, "text", "flyInRight", 1);
+  title.animations[0].props.to = title.x;
   items.push(title);
 
-  // Bullets
-  const baseY = 200;
+  // Bullet list
+  const baseY = title.y + gapBelowHeading;
   data.bullets.forEach((b, i) => {
     const bulletData = T.applyPreset(T.stylePresets.text.bullet, {
       text: b.text || b,
-      y: baseY + i * 60,
+      y: baseY + i * lineGap,
       x: 100,
       showAt: b.showAt ?? i + 1
     });
