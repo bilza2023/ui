54fe5751dc22c91a61940cdfb0125d4a2d631ac7
bilza2023 <bilza2023@gmail.com>
2025-12-24 20:39:34 +0500

    ok

--- build/server/chunks/prisma-CbVrW2fI.js
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = globalThis.__taleemPrisma ?? new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.__taleemPrisma = prisma;
-
-export { prisma as p };
-//# sourceMappingURL=prisma-CbVrW2fI.js.map
